32eb5b3e8e394f2c953ac76a2a2a7c594bea5827
Thomas Thune Hansen <tth@bellcom.dk>
2014-03-19 15:47:13 +0100

    leftovers

--- user_delete.php
+++ user_delete.php
@@ -1,5 +1,5 @@
 <?php
-$users_query = db_query('SELECT u.uid FROM {users} u WHERE u.uid > :uid', array(':uid' => '75'));
+$users_query = db_query('SELECT u.uid FROM {users} u WHERE u.uid > :uid', array(':uid' => '80'));
 $result = $users_query->fetchAll();
 
 foreach($result as $uid){
